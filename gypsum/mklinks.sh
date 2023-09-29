65f3915b5d1ff43720642836a09d5586028c5093
Noah Burghardt <burghardtnoah@gmail.com>
2023-09-28 19:27:21 -0600

    started new config for gypsum

---
+++ gypsum/mklinks.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/gypsum/.config/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/gypsum/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/gypsum/.config/picom.conf ~/.config/picom.conf
+ln -s ~/dotfiles/gypsum/.Xresources ~/.Xresources
+ln -s ~/dotfiles/gypsum/.config/rofi ~/.config/rofi
