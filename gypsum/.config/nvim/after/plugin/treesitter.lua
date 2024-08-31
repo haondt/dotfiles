6198b93df1c98c9e3556740c4faf76f1c603aca1
Noah Burghardt <burghardtnoah@gmail.com>
2024-08-31 09:29:30 -0600

    disabled dockerfile ts

--- gypsum/.config/nvim/after/plugin/treesitter.lua
+++ gypsum/.config/nvim/after/plugin/treesitter.lua
@@ -21,6 +21,9 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
+    disable = {
+        "dockerfile" -- has issues currently
+    },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
