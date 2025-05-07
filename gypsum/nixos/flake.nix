b55f0c728eb30deeaa8c43a5db294806a22dde8b
haondt <code@mail.haondt.dev>
2025-05-07 12:57:24 +0000

    adding nixos

---
+++ gypsum/nixos/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A simple NixOS flake";
+  inputs = {
+	 	nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; 
+	};
+	outputs = { self, nixpkgs, ...}@inputs: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+			];
+		};
+
+	};
+}
