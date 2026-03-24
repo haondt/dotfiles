a7933e94e05dc3fb20d9e42fe084515b8c5e2c3e
haondt <code@mail.haondt.dev>
2026-03-23 20:49:24 -0600

    apparently you can just disable the easy-dotnet lsp

--- kunzite/ansible/roles/neovim-configure/files/nvim/lua/lazy-plugins/easy-dotnet.lua
+++ kunzite/ansible/roles/neovim-configure/files/nvim/lua/lazy-plugins/easy-dotnet.lua
@@ -43,50 +43,10 @@ return {
                 end
             end
         },
-        lsp = {
-            config = {
-                settings = {
-                    ["csharp|code_lens"] = {
-                        dotnet_enable_references_code_lens = false,
-                        dotnet_enable_tests_code_lens = false,
-                    },
-                    ["csharp|background_analysis"] = {
-                        ["background_analysis.dotnet_analyzer_diagnostics_scope"] = "fullSolution",
-                        ["background_analysis.dotnet_compiler_diagnostics_scope"] = "fullSolution",
-                    }
-                }
-            }
-        }
+        lsp = { enabled = false }
     },
     keys = {
         { '<leader>pt', '<cmd>Dotnet testrunner<cr>', desc = '[p]roject [t]ests' },
     },
-    ft = { "cs", "razor" },
-    config = function(_, opts)
-        -- disable easy_dotnet diagnostics in favor of roslyn
-        local orig_diag = vim.lsp.handlers["textDocument/diagnostic"]
-        vim.lsp.handlers["textDocument/diagnostic"] = function(err, result, ctx, config)
-            local client = vim.lsp.get_client_by_id(ctx.client_id)
-            if client and client.name == "easy_dotnet" then return end
-            orig_diag(err, result, ctx, config)
-        end
-        require('easy-dotnet').setup(opts)
-
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(args)
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if client and client.name == "easy_dotnet" then
-                    client.server_capabilities.hoverProvider = nil
-                    client.server_capabilities.codeLensProvider = nil
-                    client.server_capabilities.completionProvider = nil
-                    client.server_capabilities.signatureHelpProvider = nil
-                    client.server_capabilities.definitionProvider = nil
-                    client.server_capabilities.referencesProvider = nil
-                    client.server_capabilities.inlayHintProvider = nil
-                    client.server_capabilities.diagnosticProvider = nil
-                    client.server_capabilities.renameProvider = nil
-                end
-            end,
-        })
-    end
+    ft = { "cs", "razor" }
 }
