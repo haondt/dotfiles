6545c5d006704041a2e421cee32979abc3f3b675
haondt <code@mail.haondt.dev>
2026-03-27 14:21:13 -0600

    disable flash search

--- kunzite/ansible/roles/neovim-configure/files/nvim/lua/lazy-plugins/flash.lua
+++ kunzite/ansible/roles/neovim-configure/files/nvim/lua/lazy-plugins/flash.lua
@@ -1,9 +1,7 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {
-        modes = { search = { enabled = true } }
-    },
+    opts = {},
     keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
